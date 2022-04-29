e2b86d505b7dc189df11f57b73f0d58569886cd9
guilherme <guigege14@hotmail.com>
2022-04-29 07:16:32 -0700

    commit inicial

---
+++ ifElse.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.ifelse;
+
+import java.util.scanner
+public class ifElse { 
+public static void main(String[] args){
+
+Scanner input = new Scanner (System.in);
+float N1, N2, N3;
+
+System.out.print)("digite um numero: ");
+N1 = input.nextfloat();
+
+
+
+}
+
+}
